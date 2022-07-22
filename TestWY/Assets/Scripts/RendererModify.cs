ba4976248a953b2addf2c06419052cdb6308117e
linyanhou <linyanhou@qq.com>
2022-07-22 10:21:06 +0800

    debug csm

--- TestWY/Assets/Scripts/RendererModify.cs
+++ TestWY/Assets/Scripts/RendererModify.cs
@@ -1,22 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
+// 这个脚本挂相机上测试
+[ExecuteAlways]
 public class RendererModify : MonoBehaviour
 {
-    private UniversalAdditionalCameraData _cameraData;
-    // Start is called before the first frame update
-    void Start()
+    void OnDrawGizmosSelected()
     {
-        _cameraData = GetComponent<UniversalAdditionalCameraData>();
+        UniversalAdditionalCameraData _cameraData = GetComponent<UniversalAdditionalCameraData>();
         ForwardRenderer renderer = (ForwardRenderer)_cameraData.scriptableRenderer;
-        //renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[0];
-    }
+        Vector3[] pos = new Vector3[] {
+            new Vector3(renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[0].x,
+            renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[0].y,
+            renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[0].z),
+            new Vector3(renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[1].x,
+            renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[1].y,
+            renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[1].z),
+            new Vector3(renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[2].x,
+                renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[2].y,
+                renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[2].z),
+            new Vector3(renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[3].x,
+                renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[3].y,
+                renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[3].z)
+        };
+        float[] r = new []
+        {
+            renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[0].w,
+            renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[1].w,
+            renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[2].w,
+            renderer.m_MainLightShadowCasterPass.m_CascadeSplitDistances[3].w
+        } ;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(pos[0],r[0]);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(pos[1],r[1]);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(pos[2],r[2]);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(pos[3],r[3]);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
